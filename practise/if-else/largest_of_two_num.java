06e72eef75ee34417ae63d64b1ea32dfd2e08b04
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-25 06:35:14 +0545

    Largest of Two Numbers

---
+++ practise/if-else/largest_of_two_num.java
@@ -0,0 +1,10 @@
+public class largest_of_two_num {
+    public static void main(String[] args) {
+        int x = -5, y = -4;
+        if (x > y) {
+            System.out.println(x + " is the largest number.");
+        } else {
+            System.out.println(y + " is the largest number.");
+        }
+    }
+}
