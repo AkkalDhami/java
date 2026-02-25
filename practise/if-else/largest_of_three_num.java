5182394ce74fa6333b6841703f0efc4b3f837538
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-25 06:37:14 +0545

    Largest of Three Numbers

---
+++ practise/if-else/largest_of_three_num.java
@@ -0,0 +1,12 @@
+public class largest_of_three_num {
+    public static void main(String[] args) {
+        int x = -5, y = 9, z = 9;
+        if (x > y) {
+            System.out.println(x + " is the largest number.");
+        } else if (y > z) {
+            System.out.println(y + " is the largest number.");
+        } else {
+            System.out.println(z + " is the largest number.");
+        }
+    }
+}
