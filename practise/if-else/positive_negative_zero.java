c9a13da5c12f76c3a6583533eb7c8173731ad241
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-25 06:33:49 +0545

    Write a program that checks whether a number is: - Positive - Negative - Zero

---
+++ practise/if-else/positive_negative_zero.java
@@ -0,0 +1,13 @@
+
+public class positive_negative_zero {
+    public static void main(String[] args) {
+        int num = -6;
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+    }
+}
