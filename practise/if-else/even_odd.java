3dafd43a0c227cf6cb566e7eccc724b7e393b8b4
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-25 06:30:44 +0545

    find even or odd

---
+++ practise/if-else/even_odd.java
@@ -0,0 +1,11 @@
+
+public class even_odd {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num % 2 != 0) {
+            System.out.println(num + " is odd number.");
+        } else {
+            System.out.println(num + " is even number.");
+        }
+    }
+}
