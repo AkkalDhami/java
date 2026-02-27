e579e5d12837fdce2194a8fbc74c05f2e8cce627
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-27 09:28:59 +0545

    break and continue

--- 01-basics/break_continue.java
+++ 01-basics/break_continue.java
@@ -18,7 +18,7 @@ public class break_continue {
 
         //? labels
         outer: for (int i = 1; i <= 10; i++) {
-            inner: for (int j = 1; j <= 10; j++) {
+            for (int j = 1; j <= 10; j++) {
                 if (j == 5) {
                     continue outer;
                 }
