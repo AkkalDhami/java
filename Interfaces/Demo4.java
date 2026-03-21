82c2a201515675c491923dd0c94f5d935b34ed28
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-21 16:20:29 +0545

    Multiple Inheritance

---
+++ Interfaces/Demo4.java
@@ -0,0 +1,30 @@
+public class Demo4 {
+    public static void main(String[] args) {
+        C c = new C();
+        c.print();
+        c.print2();
+    }
+}
+
+//? Multiple Inheritance
+
+interface A {
+    void print();
+}
+
+interface B {
+    void print2();
+}
+
+class C implements A, B {
+    @Override
+    public void print() {
+        System.out.println("A");
+    }
+
+    @Override
+    public void print2() {
+        System.out.println("B");
+    }
+}
+
