dd3239d18a1071fb834ed19a68748455400a670b
AkkalDhami <dhamiakkal21@gmail.com>
2026-03-21 16:18:35 +0545

    day-24: variables inside interface

--- Interfaces/Demo3.java
+++ Interfaces/Demo3.java
@@ -1,11 +1,28 @@
 public class Demo3 {
     public static void main(String[] args) {
         System.out.println(Math.PI);
+        System.out.println(Math.VALUE);
+
+        Random r = new Random();
+        r.fun();
+
+        Math m = new Random();
+        m.fun();
     }
 }
 
-//? variables inside interface
+// ? variables inside interface
 
 interface Math {
-   public static final double PI = 3.14;
+    public static final double PI = 3.14;
+    int VALUE = 10; //? automatically static final
+
+    void fun();
+}
+
+class Random implements Math {
+    @Override
+    public void fun() {
+        System.out.println(PI);
+    }
 }
