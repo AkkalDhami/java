b685951b801a5cc9eb5f168292d0d89c90fa9168
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-10 09:35:09 +0545

    day-34: HashMap, LinkedHashMap

---
+++ MapSet/Demo4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Demo4 {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        Map<String, Integer> map2 = new LinkedHashMap<>(100);
+
+        Map<String, Integer> map3 = new HashMap<>(100, 0.3f);
+
+        Map<String, Integer> map4 = new HashMap<>(map3);
+
+        map.put("Akkal", 30);
+        map.put("Aavash", 25);
+        map.put("Akkal", 35);
+
+        System.out.println(map);
+    }
+}
